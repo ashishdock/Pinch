4fa3da705ee07e98c95e85e5057ec01f5f9e1486
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-30 11:42:37 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ashish Sharma on 12/30/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
